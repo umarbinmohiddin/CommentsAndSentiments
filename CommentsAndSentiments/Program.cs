dd5c29c343891423692a40bbc338a58acc6d8f1b
HP <umarbinmohiddin786@gmail.com>
2023-03-06 18:39:03 +0530

    Add project files.

---
+++ CommentsAndSentiments/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CommentsAndSentiments;
+
+
+            Console.WriteLine("Please Enter The Comment");
+
+            string userdata = Console.ReadLine();
+            //Load sample data
+            var sampleData = new CommentSentimentModel.ModelInput();
+            sampleData.Review = userdata;
+         
+
+            //Load model and predict output
+            var result = CommentSentimentModel.Predict(sampleData);
+
+            Console.Write("Sentiment of your Comment is   ");
+            Console.WriteLine(result.PredictedLabel);
+
+
+
+
+
+
